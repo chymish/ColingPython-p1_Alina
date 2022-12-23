429f35f46af698b1aa365f8745592dca1297d8aa
pino <st079877@student.spbu.ru>
2022-12-24 01:22:46 +0300

    1st task of HW2

--- HW2/A/A.py
+++ HW2/A/A.py
@@ -2,7 +2,7 @@ def solution(arr):
     if len(arr) == 0: #проверили, что непустая
         return 0
 
-    c = arr[0]
+    c = arr[0] #символ, с которого мы начинаем
     cur_seq = 0 #current sequence
     max_seq = 0
 
@@ -17,7 +17,6 @@ def solution(arr):
     max_seq = max(max_seq, cur_seq)
 
     return max_seq
-
 
 if __name__ == "__main__":
     solution(input())
