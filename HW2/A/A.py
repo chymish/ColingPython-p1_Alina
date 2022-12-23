ec9f5e8439d80826b7865d89700c45890af323b1
pino <st079877@student.spbu.ru>
2022-12-24 01:15:03 +0300

    1st task of HW2

--- HW2/A/A.py
+++ HW2/A/A.py
@@ -1,3 +1,27 @@
 def solution(arr):
-    ### YOUR CODE ###
-    return
+    if len(arr) == 0: #проверили, что непустая
+        return 0
+
+    c = arr[0]
+    cur_seq = 0 #current sequence
+    max_seq = 0
+
+    for x in arr:
+        if x == c:
+            cur_seq += 1
+
+        else:
+            max_seq = max(max_seq, cur_seq)
+            cur_seq = 1
+            c = x
+    max_seq = max(max_seq, cur_seq)
+
+    return max_seq
+
+
+if __name__ == "__main__":
+    solution(input())
+
+#Напишите функцию, принимающую массив arr и возвращающую значение максимального количества
+#подряд идущих одинаковых элементов.
+#Ваше решение должно быть написано одним циклом.
